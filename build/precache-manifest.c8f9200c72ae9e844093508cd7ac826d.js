f7ba0f53ffe429f4c8cc4df190c647af3b2d6929
izanzar omar <izanzar.omar@gmail.com>
2022-10-10 12:56:53 +0100

    changing the server url

---
+++ build/precache-manifest.c8f9200c72ae9e844093508cd7ac826d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bda529897ce8cb51686e1ed0fea720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839d64d96b3052f2762d",
+    "url": "/static/css/main.f01d3040.chunk.css"
+  },
+  {
+    "revision": "6987e179187d0b1fd141",
+    "url": "/static/js/2.7b9df61e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b9df61e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839d64d96b3052f2762d",
+    "url": "/static/js/main.be8ff193.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
